94e99c4b8cdfc5003d6df0f0bc60279d6709df3b
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-09-17 14:04:30 -0300

    :sparkles: Finish socket implementation.

--- index.js
+++ index.js
@@ -12,25 +12,35 @@ const board = new five.Board()
 board.on('ready', () => {
   const relays = five.Relays([5, 6, 7, 8])
 
-  socket.on('toggle', data => {
-    let message
+  socket.emit('specify client', {type: 'arduinoHandler'})
 
-    if (data.target === 'toggleAll') {
-      relays.toggle()
-      message = 'Toggled all relays.'
-    } else if (data.target === 'openAll') {
-      relays.open()
-      message = 'Opened all relays.'
-    } else if (data.target === 'closeAll') {
-      relays.close()
-      message = 'Closed all relays.'
+  socket.on('action', data => {
+    let target
+    let message
+    let targetMessage
+
+    if (data.target === 'all') {
+      target = relays
+      targetMessage = 'all relays.'
     } else if (data.target in relays) {
-      relays[data.target].toggle()
-      message = `Toggled relay ${data.target}.`
+      target = relays[data.target]
+      targetMessage = `relay ${data.target}.`
     } else {
-      message = `Did not find the relay ${data.target}.`
+      socket.emit('response', {message: `Did not find the relay ${data.target}.`})
+      return
     }
 
+    message = capitalizeFirstLetter(data.action) + (data.action === 'open' ? 'ed' : 'd') + ` ${targetMessage}`
+    target[data.action]()
     socket.emit('response', {message})
   })
 })
+
+/**
+ * Capitalize first letter of the word or phrase
+ * @param {String} string
+ * @returns {String}
+ */
+function capitalizeFirstLetter (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
