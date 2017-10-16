0d01634d5c18cf064bd4a15dd7e6c64ffa4fd508
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-10-16 03:44:54 -0300

    :sparkles: Mock board through command parameter.

--- index.js
+++ index.js
@@ -7,7 +7,23 @@ if (!process.env.SOCKET_HOST) {
 const socket = require('socket.io-client').connect(process.env.SOCKET_HOST)
 const five = require('johnny-five')
 
-const port = process.env.PORT
-const board = port ? new five.Board({port}) : new five.Board()
+const boardOptions = {}
+
+if (process.argv.indexOf('--mock-board') !== -1) {
+  boardOptions.io = new (require('mock-firmata').Firmata)()
+  setTimeout(() => {
+    board.emit('ready')
+    process.on('exit', () => {
+      board.emit('close')
+    })
+  }, 500) // Give it half of a second to set the event listeners from "events.js"; then, trigger "ready"
+} else {
+  const port = process.env.PORT
+  if (port) {
+    boardOptions.port = port
+  }
+}
+
+const board = new five.Board(boardOptions)
 
 require('./events')({socket, board})
